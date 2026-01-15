562811c6b60b8f5bc6fb0e144c92add1ad746cc1
Chinh Đặng <polaris0512@users.noreply.huggingface.co>
2026-01-15 03:46:58 +0000

    Change port number to fit the deployment environment. (PORT: 7860)

--- main.py
+++ main.py
@@ -391,5 +391,5 @@ def ping():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 7860))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
